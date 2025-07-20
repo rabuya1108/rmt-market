ad3405f81611c094c259d9bc0de8f3da14ad40ef
rabuya1108 <rabuya1108@gmail.com>
2025-07-20 15:30:22 +0900

    firebase.js の更新

--- app/firebase.js
+++ app/firebase.js
@@ -1,19 +1,18 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore"; // ← 追加！
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC-vljhQcGPgd3g4VLLUN5xkIzD0NTVNV8",
-  authDomain: "rmt-market.firebaseapp.com",
-  projectId: "rmt-market",
-  storageBucket: "rmt-market.firebasestorage.app",
-  messagingSenderId: "573324963079",
-  appId: "1:573324963079:web:ff9109e3bee9710ff529a2",
-  measurementId: "G-219L8N300W"
+  apiKey: "あなたのAPIキー",
+  authDomain: "あなたのプロジェクト.firebaseapp.com",
+  projectId: "あなたのプロジェクトID",
+  storageBucket: "あなたのプロジェクト.appspot.com",
+  messagingSenderId: "送信者ID",
+  appId: "アプリID"
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore(app); // ← Firestoreインスタンス作成
 
-export { app, db }; // ← dbをエクスポート
+export const db = getFirestore(app);
+export const storage = getStorage(app);

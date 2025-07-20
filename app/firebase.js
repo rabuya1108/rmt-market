62d9af5dc7f6d11adac7a71c096fbc86b174f908
rabuya1108 <rabuya1108@gmail.com>
2025-07-20 17:28:50 +0900

    firebase.js の更新

--- app/firebase.js
+++ app/firebase.js
@@ -1,18 +1,15 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+// app/firebase.js
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js';
 
 const firebaseConfig = {
-  apiKey: "あなたのAPIキー",
-  authDomain: "あなたのプロジェクト.firebaseapp.com",
-  projectId: "あなたのプロジェクトID",
-  storageBucket: "あなたのプロジェクト.appspot.com",
-  messagingSenderId: "送信者ID",
-  appId: "アプリID"
+  apiKey: "まさしのAPIキー",
+  authDomain: "まさしのプロジェクト.firebaseapp.com",
+  projectId: "まさしのプロジェクトID",
+  storageBucket: "まさしのプロジェクト.appspot.com",
+  messagingSenderId: "xxxxxxxxxxxx",
+  appId: "1:xxxxxxxxxxxx:web:xxxxxxxxxxxxxx"
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app);
-export const storage = getStorage(app);

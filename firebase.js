b0f0965abbc3d6e21f8e24367847bf6a7fae6bf9
rabuya1108 <rabuya1108@gmail.com>
2025-07-20 12:51:45 +0900

    firebase.js の更新

--- firebase.js
+++ firebase.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore"; // ← 追加！
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC-vljhQcGPgd3g4VLLUN5xkIzD0NTVNV8",
   authDomain: "rmt-market.firebaseapp.com",
@@ -16,7 +12,8 @@ const firebaseConfig = {
   measurementId: "G-219L8N300W"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-export { app };
+const db = getFirestore(app); // ← Firestoreインスタンス作成
+
+export { app, db }; // ← dbをエクスポート
